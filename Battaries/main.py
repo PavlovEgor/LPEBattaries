2fa7c654cf9f57af1325ff890702da4338ec656a
PavlovEgor <pavlov.ep@phystech.edu>
2023-07-18 10:20:09 +0300

    first commit

---
+++ Battaries/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+batData = pd.DataFrame()
+technical_specification = pd.read_csv(r'C:\Users\Дмитрий\Downloads\аккумуляторы электромобилей - ТЗ.csv')
+print(technical_specification, 1)
+
