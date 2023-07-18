b04bcc9d904520954ac9b8e7235d4f7378eebfd5
PavlovEgor <pavlov.ep@phystech.edu>
2023-07-18 17:51:20 +0300

    update Chaker and make answer

--- Battaries/main.py
+++ Battaries/main.py
@@ -6,36 +6,38 @@ technical_specification = pd.read_csv(
     r'C:\Users\Дмитрий\PycharmProjects\pythonProject\Data\аккумуляторы электромобилей - ТЗ.csv')
 
 
-def make_prod_from_str(string):
-    return np.array(list(map(float, string.split('*')))).prod()
-
+def make_prod_from_str(string, form):  # расчет объема ячейки исходя их формы в мм3
+    if form == 'призматическая' or form == 'pouch':  # для призматических и пауч запись в файле длина*высота*толщина
+        return np.array(list(map(float, string.split('*')))).prod()
+    if form == 'цилиндрическая':  # для цилиндрических запись: диаметр * высота
+        D, h = list(map(float, string.split('*')))
+        return np.pi * h * (D / 2) ** 2
 
 class Chaker:
 
     def __init__(self, bd, ts):
-        self.batData = bd
-        self.technical_specification = ts
+        self.batData = bd  # информация о всех доступных ячейках
+        self.technical_specification = ts  # техническое задание от заказчика
 
-    def quantity_in_series(self):
+    def quantity_in_series(self):  # расчет количества ячеек в последовательности по напряжению в ТЗ и вольтажу ячейки
         n = self.technical_specification['Напряжение, В'].values / self.batData['Вольтаж, В'].values
         return np.ceil(n)
 
-    def quantity_in_parallel(self):
+    def quantity_in_parallel(self):  # расчет количества ячеек в параллели по емкости в ТЗ и емкости ячейки
         n = self.technical_specification['Емкость, Ач'].values / self.batData['Ёмкость, Ач'].values
         return np.ceil(n)
 
-    def volume_of_cell(self):
+    def volume_of_cell(self):  # расчет объемов ячеек в литрах
         V = []
         for s in self.batData['Размеры,мм']:
             v = make_prod_from_str(s) * 1e-6
             V.append(v)
         return np.array(V)
 
-    def mass(self):
+    def mass(self):  # проверка общей массы ячеек по ТЗ
 
-        if np.isnan(self.technical_specification['Масса полной батареи, кг'].values):
-            print(self.technical_specification['Масса полной батареи, кг'].values)
-            return np.array([True] * len(self.batData['Производитель']))
+        if self.technical_specification['Масса полной батареи, кг'].isnull().values[0]:  # проверка на наличие ТЗ
+            return np.array([True] * len(self.batData['Производитель']))  # Если нет ТЗ, то подходят все ячейки
 
         num_of_cells = self.quantity_in_parallel() * self.quantity_in_series()
         mass_of_cells = num_of_cells * self.batData['масса, кг']
@@ -43,37 +45,36 @@ class Chaker:
 
     def size(self):
 
-        if self.technical_specification['Габариты сборки/\nбатареи, мм'].isnull().values[0]:
-            return np.array([True] * len(self.batData['Производитель']))
+        if self.technical_specification['Габариты сборки/\nбатареи, мм'].isnull().values[0]:  # проверка на наличие ТЗ
+            return np.array([True] * len(self.batData['Производитель']))  # Если нет ТЗ, то подходят все ячейки
 
         num_of_cells = self.quantity_in_parallel() * self.quantity_in_series()
         volume_of_cells = num_of_cells * self.volume_of_cell()
-        print(self.technical_specification['Габариты сборки/\nбатареи, мм'].isnull())
-        ts_volume = make_prod_from_str(self.technical_specification['Габариты сборки/\nбатареи, мм'].values[0])
-
+        ts_volume = make_prod_from_str(self.technical_specification['Габариты сборки/\nбатареи, мм'].values[0], 'призматическая')
         return np.array(volume_of_cells) < ts_volume
 
-    def standart_check_req(self, nameOfParam):
+    def standart_check_req(self, nameOfParam, k=1):  # проверка того, что параметр ячейки больше его параметра в ТЗ
         if self.technical_specification[nameOfParam].isnull().values[0]:
             return np.array([True] * len(self.batData['Производитель']))
-        return np.array(self.batData[nameOfParam]) > float(self.technical_specification[nameOfParam].iloc[0])
+
+        return np.array(self.batData[nameOfParam]) * k > float(self.technical_specification[nameOfParam].iloc[0])
 
     def live_time(self):
-        return self.standart_check_req('Количество циклов \nна заряд/разряд')
+        return self.standart_check_req('Количество циклов на заряд/разряд')
 
     def long_current_of_discharging(self):
-        return self.standart_check_req('Длительный ток\nразряда, А')
+        return self.standart_check_req('Длительный ток разряда, А', self.quantity_in_parallel())
 
     def long_current_of_charging(self):
-        return self.standart_check_req('Длительный ток\nзаряда, А')
+        return self.standart_check_req('Длительный ток заряда, А', self.quantity_in_parallel())
 
     def fast_current_of_discharging(self):
-        return self.standart_check_req('Максимальный ток\r\nразряда, А')
+        return self.standart_check_req('Максимальный ток разряда, А', self.quantity_in_parallel())
 
     def fast_current_of_charging(self):
-        return self.standart_check_req('Максимальный ток\nзаряда, А')
+        return self.standart_check_req('Максимальный ток заряда, А', self.quantity_in_parallel())
 
-    def standart_check_eq(self, nameOfParam):
+    def standart_check_eq(self, nameOfParam):  # проверка того, что параметр ячейки равен его параметру в ТЗ
         if self.technical_specification[nameOfParam].isnull().values[0]:
             return np.array([True] * len(self.batData['Производитель']))
 
@@ -85,7 +86,6 @@ class Chaker:
     def form_factor(self):
         return self.standart_check_eq('Формфактор')
 
-
 ts = technical_specification[1:2]
 Model = Chaker(batData, ts)
 Answer = pd.DataFrame({
@@ -96,6 +96,20 @@ Answer = pd.DataFrame({
     'Габариты сборки/\nбатареи, мм': Model.size(),
     'Масса полной батареи, кг': Model.mass(),
     'Количество циклов \nна заряд/разряд': Model.live_time(),
+    'Длительный ток разряда, А': Model.long_current_of_discharging(),
+    'Длительный ток заряда, А': Model.long_current_of_charging(),
+    'Максимальный ток разряда, А': Model.fast_current_of_discharging(),
+    'Максимальный ток заряда, А': Model.fast_current_of_charging(),
 })
-print(Answer)
+
+
+AT = Answer.T
+R = []
+for i in range(len(Answer.index)):
+    r = np.array(AT[i][2:], dtype=int).sum()
+    R.append(r)
+Answer['Рейтинг по совпадениям'] = R
+Answer = Answer.sort_values(by='Рейтинг по совпадениям', ascending=False)
 Answer.to_csv('Answer.csv')
+
+
